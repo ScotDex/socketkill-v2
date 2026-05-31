69c2a8ad06733b9fb6e9e12736ec1659bcc62992
ScotDex <themadlyscientific@gmail.com>
2026-05-31 20:03:10 +0100

    adjusting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import cloudflare from '@astrojs/cloudflare'
-import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
