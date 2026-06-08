64eb2edaa2e65de54c363603bd1dd75e4c0b31f5
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2026-06-08 15:34:19 +0000

    css clean up and config defining

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import cloudflare from '@astrojs/cloudflare'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 
