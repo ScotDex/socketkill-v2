f8e543281c6ddb02b342c0fc656cafe2f2b042f0
ScotDex <themadlyscientific@gmail.com>
2026-07-23 21:52:23 +0100

    adding links

--- src/lib/entity-links.js
+++ src/lib/entity-links.js
@@ -2,11 +2,20 @@
 export function externalLink(kind, value) {
     switch (kind) {
         case 'character': return `https://zkillboard.com/character/${value}/`
-        case 'corp':      return `https://zkillboard.com/corporation/${value}/`
-        case 'alliance':  return `https://zkillboard.com/alliance/${value}/`
-        case 'system':    return `https://evemaps.dotlan.net/system/${encodeURIComponent(String(value).replace(/ /g, '_'))}`
-        case 'region':    return `https://evemaps.dotlan.net/region/${encodeURIComponent(String(value).replace(/ /g, '_'))}`
-        case 'type':      return `https://everef.net/type/${value}`
-        default:          return '#'
+        case 'corp': return `https://zkillboard.com/corporation/${value}/`
+        case 'alliance': return `https://zkillboard.com/alliance/${value}/`
+        case 'system': return `https://evemaps.dotlan.net/system/${encodeURIComponent(String(value).replace(/ /g, '_'))}`
+        case 'region': return `https://evemaps.dotlan.net/region/${encodeURIComponent(String(value).replace(/ /g, '_'))}`
+        case 'type': return `https://everef.net/type/${value}`
+        default: return '#'
+    }
+}
+
+export function internalLink(kind, value) {
+    switch (kind) {
+        case 'character': return `/pilot/${value}`
+        case 'corp': return `/corp/${value}`
+        case 'alliance': return `/alliance/${value}`
+        default: return '#'
     }
 }
